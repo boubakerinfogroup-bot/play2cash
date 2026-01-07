cc233a1556658919e2da022104cf92bedbe72cf9
beuzdigital-del <beuzdigital@gmail.com>
2026-01-07 18:39:41 +0100

    fix: change game to gameData in db seed

--- app/api/setup-admin/route.ts
+++ app/api/setup-admin/route.ts
@@ -92,7 +92,7 @@ export async function GET(request: Request) {
                     description: gameData.description,
                     descriptionAr: gameData.descriptionAr
                 },
-                create: game
+                create: gameData
             })
         }
 
